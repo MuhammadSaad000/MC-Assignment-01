79d05420eb1b78662e06728530a8766952840da4
Muhammad Saad <muhammadsaad6345@gmail.com>
2022-05-22 08:31:10 +0500

    Layout Added

---
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -0,0 +1,81 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity implements View.OnClickListener {
+
+    Button a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        a = findViewById(R.id.a);
+        b = findViewById(R.id.b);
+        c = findViewById(R.id.c);
+        d = findViewById(R.id.d);
+        e = findViewById(R.id.e);
+        f = findViewById(R.id.f);
+        g = findViewById(R.id.g);
+        h = findViewById(R.id.h);
+        i = findViewById(R.id.i);
+        j = findViewById(R.id.j);
+        k = findViewById(R.id.k);
+        l = findViewById(R.id.l);
+        m = findViewById(R.id.m);
+        n = findViewById(R.id.n);
+        o = findViewById(R.id.o);
+        p = findViewById(R.id.p);
+        q = findViewById(R.id.q);
+        r = findViewById(R.id.r);
+        s = findViewById(R.id.s);
+        t = findViewById(R.id.t);
+        u = findViewById(R.id.u);
+        v = findViewById(R.id.v);
+        w = findViewById(R.id.w);
+        x = findViewById(R.id.x);
+        y = findViewById(R.id.y);
+        z = findViewById(R.id.z);
+
+        a.setOnClickListener(this);
+        b.setOnClickListener(this);
+        c.setOnClickListener(this);
+        d.setOnClickListener(this);
+        e.setOnClickListener(this);
+        f.setOnClickListener(this);
+        g.setOnClickListener(this);
+        h.setOnClickListener(this);
+        i.setOnClickListener(this);
+        j.setOnClickListener(this);
+        k.setOnClickListener(this);
+        l.setOnClickListener(this);
+        m.setOnClickListener(this);
+        n.setOnClickListener(this);
+        o.setOnClickListener(this);
+        p.setOnClickListener(this);
+        q.setOnClickListener(this);
+        r.setOnClickListener(this);
+        s.setOnClickListener(this);
+        t.setOnClickListener(this);
+        u.setOnClickListener(this);
+        v.setOnClickListener(this);
+        w.setOnClickListener(this);
+        x.setOnClickListener(this);
+        y.setOnClickListener(this);
+        z.setOnClickListener(this);
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
